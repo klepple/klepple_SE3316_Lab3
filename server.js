76407dcffadc5413dcce4a0fbf8e07afee44b74d
Kahlia Lepple <klepple@uwo.ca>
2017-11-06 02:53:34 +0000

    Basic working version changes

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ var app = express();  // define our app using express
 var bodyParser = require('body-parser');  // configure app to use bodyParser()
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:28017/messageBoard', { useMongoClient: true}); // connect to our database
+mongoose.connect('mongodb://localhost:27017/dolphins', { useMongoClient: true}); // connect to our database
 
-var Message = require('./message.js');
+var Dolphin = require('./message.js');
 
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +21,6 @@ var port = 8081; // set our port
 // ------------------ ROUTES FOR OUR API ---------------------
 
 var router = express.Router();  // get an instance of the express Router
-
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -34,64 +33,81 @@ router.use(function(req, res, next) {
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
-// serve files in static' folder at root URL '/'
-app.use('/', express.static('8081'));
 
 // on routes that end in /messages
 router.route('/messages')
 
-    // create a message (accessed at POST http://localhost:8080/api/messages)
+    // create a dolphin (accessed at POST http://localhost:8080/api/dolphins)
     .post(function(req, res) {
-
-        var message = new Message(); // create a new instance of the Message model
-        message.courseCode = req.body.courseCode; // set the courseCode
-        message.content = req.body.content; //set the content
-        message.timeStamp = req.body.timeStamp; //set the timeStamp
-        
-        //Set header to avoid CORS error
         res.setHeader("Access-Control-Allow-Origin", "*");
-        
-        // save the message and check for errors
-        message.save(function(err) {
+        var dolphin = new Dolphin(); // create a new instance of the Dolphin model
+        dolphin.courseCode = req.body.courseCode; // set the dolphins name (comes from the request)
+        dolphin.content = req.body.content;
+        dolphin.timeStamp = req.body.timeStamp;
+
+        // save the dolphin and check for errors
+        dolphin.save(function(err) {
             if (err) {
                 res.send(err);
             }
-            console.log("message created!");
-            res.json({ message: 'Message successfully created!' });
+
+            res.json({ message: 'Dolphin created!' });
         });
 
     })
     
-    // get all the messages (accessed at GET http://localhost:8081/api/messages)
+    // get all the dolphins (accessed at GET http://localhost:8080/api/dolphins)
     .get(function(req, res) {
-        //Set header to avoid CORS error
         res.setHeader("Access-Control-Allow-Origin", "*");
-        Message.find(function(err, messageBoard) {
+        Dolphin.find(function(err, dolphins) {
             if (err) {
                 res.send(err);
             }
-            res.json(messageBoard);
+
+            res.json(dolphins);
         });
     });
     
-// on routes that end in /messages/:message_id
 router.route('/messages/:message_id')
 
-    // get the message with that id (accessed at GET http://localhost:8080/api/messages/:message_id)
+    // get the bear with that id (accessed at GET http://localhost:8080/api/dolphins/:dolphin_id)
     .get(function(req, res) {
-        Message.findById(req.params.message_id, function(err, message) {
+        Dolphin.findById(req.params.dolphin_id, function(err, dolphin) {
             if (err){
                 res.send(err);
             }
-            res.json(message);
+            res.json(dolphin);
+        });
+    })
+    
+    // update the dolphin with this id (accessed at PUT http://localhost:8080/api/dolphins/:dolphin_id)
+    .put(function(req, res) {
+
+        // use our dolphin model to find the dolphin we want
+        Dolphin.findById(req.params.dolphin_id, function(err, dolphin) {
+
+            if (err){
+                res.send(err);
+            }
+
+            dolphin.name = req.body.name;  // update the dolphin's info
+
+            // save the dolphin
+            dolphin.save(function(err) {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.json({ message: 'Dolphin updated!' });
+            });
         });
     })
         
-    // delete the message with this id (accessed at DELETE http://localhost:8080/api/messages/:message_id)
+    // delete the dolphin with this id (accessed at DELETE http://localhost:8080/api/dolphins/:dolphin_id)
     .delete(function(req, res) {
-        Message.remove({
-            _id: req.params.message_id
-        }, function(err, message) {
+        Dolphin.remove({
+            _id: req.params.dolphin_id
+        }, function(err, dolphin) {
             if (err) {
                 res.send(err);
             }
